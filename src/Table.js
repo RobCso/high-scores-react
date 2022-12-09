5016e2a9a6c4d9c1ae7bf31e854bde3a17167bb5
RobCso <robcso@DESKTOP-AA47BN0.localdomain>
2022-12-09 23:01:32 +0000

    refactor table

--- src/Table.js
+++ src/Table.js
@@ -1,9 +1,31 @@
 import React from "react";
 
 const Table = (props) => {
+  props.results.sort((a, b) =>
+    a.name > b.name ? 1 : b.name > a.name ? -1 : 0
+  );
   return (
     <div className="App">
       <table className="table">
+        {props.results          
+          .map((item, index) => (
+            <thead className="country-block">
+              <caption className="caption">High Scores : {item.name}</caption>
+              {item["scores"].sort((a, b) =>
+                a.s < b.s ? 1 : b.s < a.s ? -1 : 0
+              ).map((score, index) => {
+                return (
+                  <tr className="name-scores">
+                    <th>{score.n}</th>
+                    <th>{score.s}</th>
+                  </tr>
+                );
+              })}
+            </thead>
+          ))}
+      </table>
+
+      {/* <table className="table">
         <thead>
           <tr>
             <th scope="col">High Scores per Country</th>
@@ -15,7 +37,7 @@ const Table = (props) => {
             .map((item, index) => {
               return (
                 <tr key={index} className="country-block">
-                  <tr>High Scores: {item.name}</tr>
+                  <caption>High Scores: {item.name}</caption>
                   {item["scores"]
                     .sort((a, b) => (a.s < b.s ? 1 : b.s < a.s ? -1 : 0))
                     .map((score, index) => {
@@ -25,18 +47,11 @@ const Table = (props) => {
                         </tr>
                       );
                     })}
-                  
                 </tr>
               );
             })}
-        </tbody>
-        {/* <tbody>
-          <tr>
-            <th>three</th>
-            <th>four</th>
-          </tr>
-        </tbody> */}
-      </table>
+        </tbody>        
+      </table> */}
     </div>
   );
 };
